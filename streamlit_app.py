b01218199001ee07f8ac29fb7f5d0a8f73153e42
KRISHn369 <137753635+KRISHn369@users.noreply.github.com>
2023-06-26 11:29:12 -0500

    Update streamlit_app.py
    added Fruit Smoothie Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('🥣 🥗 🐔 🥑🍞 Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
