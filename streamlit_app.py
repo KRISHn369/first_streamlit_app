daed3fb10bec25bcb5a1f0115b40913f4cf40eb1
KRISHn369 <137753635+KRISHn369@users.noreply.github.com>
2023-07-01 23:19:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,4 +63,4 @@ if streamlit.button('Add a fruit to the list'):
   streamlit.text(back_from_function)
   streamlit.write('Thanks for adding', add_my_fruit)
   #streamlit.stop()
-my_cur.execute("insert into fruit_load_list values ('" + 'jackfruit' + "')")
+#my_cur.execute("insert into fruit_load_list values ('" + 'jackfruit' + "')")
